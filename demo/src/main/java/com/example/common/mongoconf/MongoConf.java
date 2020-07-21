aa65c3ea09031c5ee7b46f315f388b7340927a53
allblue-online/silingchao <s1141002356@dingtalk.com>
2020-07-21 10:32:59 +0800

    mysql

--- demo/src/main/java/com/example/common/mongoconf/MongoConf.java
+++ demo/src/main/java/com/example/common/mongoconf/MongoConf.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import java.util.List;
 
 /**
  * @author Si LingChao
--- demo/src/main/java/com/example/common/sqlconf/DataSource1.java
+++ demo/src/main/java/com/example/common/sqlconf/DataSource1.java
@@ -1,10 +1,8 @@
 package com.example.common.sqlconf;
-
-import com.sun.org.apache.bcel.internal.generic.NEW;
-import com.sun.org.apache.regexp.internal.RE;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -13,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
